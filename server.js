5a56df0326c00eed3e3b4ea444a17312a438977a
jszvitor <jszvitor@gmail.com>
2020-06-10 21:14:57 -0300

    Feat: Installed require-dir and product model created.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Init App
 const app = express();
@@ -9,6 +10,10 @@ mongoose.connect(
   "mongodb://localhost:27017/nodeapi",
   {useNewUrlParser: true, useUnifiedTopology: true}
 );
+requireDir('./src/models');
+
+
+const Product = mongoose.model('Product');
 
 
 // first route
@@ -17,6 +22,12 @@ app.get('/', (request, response) => {
   // What can we access in the "request", ( parameters, body, header, user, authentication, ip ...)
   // "response" is a response that is given to a request made on that route.
   // So the flow in a server is basically 'request' and 'response'.
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: "http://github.com/facebook/react-native"
+  });
+
   response.send('Hello!🎭');
 })
 
