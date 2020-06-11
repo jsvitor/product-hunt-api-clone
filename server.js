cd12086251030e44d576172540e12183425dc30c
jszvitor <jszvitor@gmail.com>
2020-06-10 23:42:36 -0300

    Feat: added and configured CORS package.
    CORS (Cross-origin resource sharing).
    That can be used to enable CORS with various options.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 // Init App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Init DB
 mongoose.connect(
