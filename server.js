df8052f00696ca4289d64b96c1e1447817bb52ad
jszvitor <jszvitor@gmail.com>
2020-06-10 21:55:51 -0300

    Feat: file restructuring

--- server.js
+++ server.js
@@ -13,23 +13,11 @@ mongoose.connect(
 requireDir('./src/models');
 
 
-const Product = mongoose.model('Product');
 
 
 // first route
-app.get('/', (request, response) => {
-  // request is the request we make to the server.
-  // What can we access in the "request", ( parameters, body, header, user, authentication, ip ...)
-  // "response" is a response that is given to a request made on that route.
-  // So the flow in a server is basically 'request' and 'response'.
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: "http://github.com/facebook/react-native"
-  });
+app.use('/api', require('./src/routes')); // o use é como se fosse um coringa. Ele vai receber todos os tipos de requisição.
 
-  response.send('Hello!🎭');
-})
 
 
 app.listen(3001);
